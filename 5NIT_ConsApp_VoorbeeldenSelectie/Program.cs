ea25e8222609c5f55ecdb2918cb50abba81e7d1a
Tom Gillade <tom.gillade@leraar.smi-aalst.be>
2020-10-01 09:43:57 +0200

    Add project files.

---
+++ 5NIT_ConsApp_VoorbeeldenSelectie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5NIT_ConsApp_VoorbeeldenSelectie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
